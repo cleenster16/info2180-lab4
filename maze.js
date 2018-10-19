9d017a718477f2f58bf31d865f2057b7f8cb75aa
Cleon Foster <cleenster16@yahoo.com>
2018-10-19 14:54:52 -0500

    Make all boundaries turn red on mouseover

--- maze.js
+++ maze.js
@@ -1,12 +1,16 @@
 window.onload = function() {
     
     var firstBoundary = document.getElementById("boundary1");
-    var boundary = document.getElementsByClassName("boundary");
+    var boundary = document.querySelectorAll(".boundary");
     var start = document.querySelector("#start");
     var end = document.querySelector("#end");
     
     let boundary1 = document.querySelector("#boundary1");
-    boundary1.addEventListener("mouseover", onBoundaryMouseOver);
+    
+    for (let i=0; i<boundary.length; i++){
+        boundary[i].addEventListener("mouseover", onBoundaryMouseOver);
+
+    }
 
     function onBoundaryMouseOver(){
         this.classList.add('youlose');

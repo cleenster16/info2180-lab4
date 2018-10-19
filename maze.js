13b8723c05b224e348c45a5dd8313cb893f94027
Cleon Foster <cleenster16@yahoo.com>
2018-10-19 16:31:38 -0500

    Disallow cheating in the maze

--- maze.js
+++ maze.js
@@ -4,16 +4,18 @@ window.onload = function() {
     let start = document.querySelector("#start");
     let end = document.querySelector("#end");
     let status = document.getElementById("status");
+    let maze = document.getElementById("maze");
     let winStatus = true;
+    let gameStart = true;
     
     for (let i=0; i<boundary.length; i++){
         boundary[i].addEventListener("mouseover", onBoundaryMouseOver);
     }
 
     start.addEventListener("click", onStartClick);
-
     end.addEventListener("mouseover", onEndMouseOver);
-
+    maze.addEventListener("mouseleave", onMazeLeave);
+    
     function onBoundaryMouseOver(){
         this.classList.add('youlose');
         if (winStatus === true){
@@ -24,19 +26,29 @@ window.onload = function() {
     function onEndMouseOver(){
         if (winStatus === true){
             status.innerHTML = "You Win!";
+            gameStart = false;
         }else{
             status.innerHTML = "You Lose!";
         }
     }
 
     function onStartClick(){
-        
+        status.innerHTML = 'Move your mouse over the "S" to begin.';
+        gameStart = true;
         for (let i=0; i<boundary.length; i++){
             if (boundary[i].classList.contains('youlose')){
                 boundary[i].classList.remove('youlose');
-                status.innerHTML = 'Move your mouse over the "S" to begin.'; 
                 winStatus = true;
             }
+        }
+    }
+
+    function onMazeLeave(){
+        if (gameStart){
+            boundary.forEach(element => {
+                element.classList.add("youlose");
+            })
+            status.innerHTML = "You Lose!";
         }
     }
 
